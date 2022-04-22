45ba12862019dc01f1c6f02c0b45137fe7ed996a
TayBatista <tayna_96.s2@hotmail.com>
2022-04-22 20:19:17 -0300

    Nova página de referencias com outros exemplos práticos no Csharp

--- Referencia_e_valores_csharp/Program.cs
+++ Referencia_e_valores_csharp/Program.cs
@@ -17,24 +17,45 @@ namespace Aula_pratica
             return x + 20;
         }
 
-        static void TrocarNome(Pessoa p1, string NomeNovo)
-        {
-            p1.Nome = NomeNovo;
-        }
-
-        public static void Main()
+        static void Demo2()
         {
             Pessoa p1 = new Pessoa();
             p1.Nome = "João";
             p1.Documento = "123.456.789-00";
             p1.Idade = 20;
 
-            Pessoa p2 = p1.Clone();           
+            Pessoa p2 = p1.Clone();
 
             TrocarNome(p1, "Maria");
 
             WriteLine($"O nome de p1 é: {p1.Nome}");
             WriteLine($"O nome de p2 é: {p2.Nome}");
+        }
+        static void TrocarNome(Pessoa p1, string NomeNovo)
+        {
+            p1.Nome = NomeNovo;
+        }
+
+        static void TrocarNome(StructPessoa p1, string NomeNovo)
+        {
+            p1.Nome = NomeNovo;
+        }
+        public static void Main()
+        {
+            StructPessoa p1 = new StructPessoa
+            {
+                Documento = "123.678.012-12",
+                Idade = 30,
+                Nome = "José"
+            };
+
+            var p2 = p1;
+
+            TrocarNome(p1, "Mario");
+
+            WriteLine($@"O nome de p1 é: {p1.Nome}
+            O nome de p2 é: {p2.Nome}"
+            );
         }
     }
 }
