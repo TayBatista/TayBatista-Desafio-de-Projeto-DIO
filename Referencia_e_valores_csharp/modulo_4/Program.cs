46a4d19392a795a9b48ab0636c3545cea8f95674
TayBatista <tayna_96.s2@hotmail.com>
2022-04-23 10:26:45 -0300

    Inserindo arquivos na pasta Módulo_4

---
+++ Referencia_e_valores_csharp/modulo_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Numero a = new Numero(2);
+Numero b = new Numero(2);
+
+
+if (a.N == b.N)
+{
+    WriteLine("A e B são iguais");
+}
+else
+{
+    WriteLine("A e B são diferentes");
+}
+
+class Numero
+{
+    public int N { get; set; }
+
+    public Numero(int n)
+    {
+        N = n;
+    }
+}
