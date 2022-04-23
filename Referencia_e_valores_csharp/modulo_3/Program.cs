949a09f0254b50d7865c57f07beb41bffb7fb2a6
TayBatista <tayna_96.s2@hotmail.com>
2022-04-23 09:52:25 -0300

    Adicionando exemplos no arquivo da pasta Modulo_3

---
+++ Referencia_e_valores_csharp/modulo_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Ricardo";
+p1.Idade = 25;
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua teste",
+    Numero = 123,
+    CEP = "12345-678",
+    Cidade = "São Paulo",
+};
+
+WriteLine("Fim do programa");
